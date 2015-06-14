a15e43e9c3ab15fba131d88a0e07b0e3fc99192e
judaszszymon <szymon.judasz@student.uj.edu.pl>
2015-06-15 00:34:16 +0200

    another fix

--- example data/4_nationalities.sql
+++ example data/4_nationalities.sql
@@ -1,30 +1,30 @@
 BEGIN;
-COPY nationalities (Nationality) FROM stdin;
-Great Britain
-United States
-Russia
-Australia
-China
-Germany
-France
-Japan
-Italy
-Spain
-Canada
-Brazil
-South Korea
-Ukraine
-Poland
-Jamaica
-Netherland
-Trinidad and Tobago
-The Bahamas
-Cayman Islands
-Zambia
-Antigua and Barbuda
-Saint Kitts and Nevis
-Ivory Coast
-Gambia
+COPY nationalities (NationalityId, Nationality) FROM stdin;
+1	Great Britain
+2	United States
+3	Russia
+4	Australia
+5	China
+6	Germany
+7	France
+8	Japan
+9	Italy
+10	Spain
+11	Canada
+12	Brazil
+13	South Korea
+14	Ukraine
+15	Poland
+16	Jamaica
+17	Netherland
+18	Trinidad and Tobago
+19	The Bahamas
+20	Cayman Islands
+21	Zambia
+22	Antigua and Barbuda
+23	Saint Kitts and Nevis
+24	Ivory Coast
+25	Gambia
 \.
 COMMIT;
 
